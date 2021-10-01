21cdbb8c0b2c3a0a5aa32338a54dd64aed2a3374
tjsgh5965 <42001961+tjsgh5965@users.noreply.github.com>
2021-10-01 15:28:25 +0900

    C++로 C Style 코딩하기 인생 1회차
    옛날 추억이 생각나서 간단하게 해봄.

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+void gotoxy(int x, int y)
+{
+	COORD pos = { x,y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+class Character {
+public:
+	string name = "¡Ú";
+	int myX = 5, myY = 5;
+	void move(int vec) {
+		switch (vec)
+		{
+		case 1: myY--;
+			break;
+		case 2: myY++;
+			break;
+		case 3: myX--;
+			break;
+		case 4: myX++;
+			break;
+		default:
+			break;
+		}
+	}
+	void eraser() {
+		gotoxy(myX * 2, myY * 2);
+		cout << "  ";
+	}
+	void print() {
+		gotoxy(myX * 2, myY * 2);
+		cout << name;
+	}
+};
+
+int main() {
+	Character me;
+	while (true)
+	{
+		me.eraser();
+		if (GetAsyncKeyState(VK_UP)) me.move(1);
+		if (GetAsyncKeyState(VK_DOWN)) me.move(2);
+		if (GetAsyncKeyState(VK_LEFT)) me.move(3);
+		if (GetAsyncKeyState(VK_RIGHT)) me.move(4);
+		me.print();
+		Sleep(50);
+	}
+}
